66a29dca3c0223986dffc786b700dc7eaee1df02
jsusilo <enr@bk.ru>
2018-06-27 22:54:41 +0500

    create the template by vue-cli

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-toolbar class="deep-purple">
+      <v-toolbar-title>Vuetify Tutorial</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+export default {
+}
